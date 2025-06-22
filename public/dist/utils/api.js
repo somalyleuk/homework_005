5323bdc91faf6b52eeba7ea5b965a1a3e52ef8f2
malyonly88 <malyonly88@gmail.com>
2025-06-22 21:47:27 +0700

    update

--- public/dist/utils/api.js
+++
@@ -1,8 +0,0 @@
-export async function fetchProducts() {
-    const res = await fetch('https://fakestoreapi.com/products');
-    return await res.json();
-}
-export async function fetchProduct(id) {
-    const res = await fetch(`https://fakestoreapi.com/products/${id}`);
-    return await res.json();
-}
